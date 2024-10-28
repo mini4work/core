ab1afd0b2e037509c4ca9aeace891fcd225d69ac
Salamandra <salamandra@devername.com>
2024-10-29 00:39:05 +0200

    hotfix

--- app/Services/ConsoleService.php
+++ app/Services/ConsoleService.php
@@ -9,23 +9,20 @@ class ConsoleService
     public function writeLine(string|array $line, ConsoleStyles $style = null): void
     {
         if (!is_array($line)) {
-            echo $style->value.$line.ConsoleStyles::ResetAllAttributes->value.PHP_EOL;
+            echo $style->value . $line . ConsoleStyles::ResetAllAttributes->value . PHP_EOL;
             return;
         }
 
         $result = '';
         foreach ($line as $text) {
-            if (isset($text[1])) {
-                if ($text[1] instanceof ConsoleStyles) {
-                    $result .= $text[1]->value.$text[0].ConsoleStyles::ResetAllAttributes->value;
-                } else {
-                    $result .= $text[1].$text[0].ConsoleStyles::ResetAllAttributes->value;
-                }
+            if (is_array($text) && isset($text[1])) {
+                $style = ($text[1] instanceof ConsoleStyles)?$text[1]->value:$text[1];
+                $result .= $style . $text[0] . ConsoleStyles::ResetAllAttributes->value;
             } else {
                 $result .= $text[0];
             }
         }
 
-        echo $result.PHP_EOL;
+        echo $result . PHP_EOL;
     }
 }
